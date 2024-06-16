c422b12f3926f27ee2b3b549f5147ddc1be62ac9
Allan Mwangi <allanmwangi800@gmail.com>
2024-06-16 11:49:26 +0300

    button function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'Join The Team';
-
   
 }
 
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+interface Image {
+  src: string;
+  alt: string;
+}
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+    images: Image[] = [
+    { src: '../../assets/memory-8141642_640.jpg', alt: 'Memory' },
+    { src: '../../assets/dvi-992630_640.jpg', alt: 'DVI Connector' },
+    { src: '../../assets/board-453758_640.jpg', alt: 'Board' },
+    { src: '../../assets/flash-memory-1306886_640.jpg', alt: 'Flash Memory' },
+    { src: '../../assets/open-hard-drive-1200164_1280.jpg', alt: 'Open Hard Drive' },
+    { src: '../../assets/turntable-1337986_640.jpg', alt: 'Turntable' }
+  ];
 
+  selectedImage: Image | null = null;
+  cart: Image[] = [];
+
+  selectImage(image: Image) {
+    this.selectedImage = image;
+  }
+
+  addToCart() {
+    if (this.selectedImage) {
+      this.cart.push(this.selectedImage);
+      console.log('Image added to cart:', this.selectedImage);
+      console.log('Current cart:', this.cart);
+      this.selectedImage = null; // Deselect image after adding to cart
+    }
+  }
 }
